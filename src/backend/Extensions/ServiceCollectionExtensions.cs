c44e61a7e2d4878fdaba1659ff2649403e4c25ea
MOROZIUK Yurii <Yurii.Moroziuk@gunvorgroup.com>
2023-11-20 00:44:15 +0200

    commit proj

---
+++ src/backend/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,94 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Azure;
+
+namespace MinimalApi.Extensions;
+
+internal static class ServiceCollectionExtensions
+{
+    private static readonly DefaultAzureCredential s_azureCredential = new();
+
+    internal static IServiceCollection AddAzureServices(this IServiceCollection services)
+    {
+        services.AddSingleton<BlobServiceClient>(sp =>
+        {
+            var config = sp.GetRequiredService<IConfiguration>();
+            var azureStorageAccountEndpoint = config["AzureStorageAccountEndpoint"];
+            ArgumentNullException.ThrowIfNullOrEmpty(azureStorageAccountEndpoint);
+
+            var blobServiceClient = new BlobServiceClient(
+                new Uri(azureStorageAccountEndpoint), s_azureCredential);
+
+            return blobServiceClient;
+        });
+
+        services.AddSingleton<BlobContainerClient>(sp =>
+        {
+            var config = sp.GetRequiredService<IConfiguration>();
+            var azureStorageContainer = config["AzureStorageContainer"];
+            return sp.GetRequiredService<BlobServiceClient>().GetBlobContainerClient(azureStorageContainer);
+        });
+
+        services.AddSingleton<SearchClient>(sp =>
+        {
+            var config = sp.GetRequiredService<IConfiguration>();
+            var (azureSearchServiceEndpoint, azureSearchIndex, key) =
+                (config["AzureSearchServiceEndpoint"], config["AzureSearchIndex"], config["AzureSearchServiceEndpointKey"]);
+
+            ArgumentNullException.ThrowIfNullOrEmpty(azureSearchServiceEndpoint);
+
+            var credential = new AzureKeyCredential(key!);
+
+            var searchClient = new SearchClient(
+                new Uri(azureSearchServiceEndpoint), azureSearchIndex, credential);
+
+            return searchClient;
+        });
+
+        services.AddSingleton<DocumentAnalysisClient>(sp =>
+        {
+            var config = sp.GetRequiredService<IConfiguration>();
+            var azureOpenAiServiceEndpoint = config["AzureDocumentIntelligenceEndpoint"] ?? throw new ArgumentNullException();
+            var key = config["AzureDocumentIntelligenceEndpointKey"] ?? throw new ArgumentNullException();
+
+            var credential = new AzureKeyCredential(key!);
+
+            var documentAnalysisClient = new DocumentAnalysisClient(
+                new Uri(azureOpenAiServiceEndpoint), credential);
+            return documentAnalysisClient;
+        });
+
+        services.AddSingleton<OpenAIClient>(sp =>
+        {
+            var config = sp.GetRequiredService<IConfiguration>();
+            var (azureOpenAiServiceEndpoint, key) = (config["AzureOpenAiServiceEndpoint"], config["AzureOpenAiServiceEndpointKey"]);
+
+            ArgumentNullException.ThrowIfNullOrEmpty(azureOpenAiServiceEndpoint);
+
+            var credential = new AzureKeyCredential(key!);
+
+            var openAiClient = new OpenAIClient(
+                new Uri(azureOpenAiServiceEndpoint), credential);
+
+            return openAiClient;
+        });
+
+        services.AddSingleton<AzureBlobStorageService>();
+        services.AddSingleton<ReadRetrieveReadChatService>();
+
+        return services;
+    }
+
+    internal static IServiceCollection AddCrossOriginResourceSharing(this IServiceCollection services)
+    {
+        services.AddCors(
+            options =>
+                options.AddDefaultPolicy(
+                    policy =>
+                        policy.AllowAnyOrigin()
+                            .AllowAnyHeader()
+                            .AllowAnyMethod()));
+
+        return services;
+    }
+}
---
+++ src/frontend/Models/LanguageDirection.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+namespace ClientApp.Models;
+
+public enum LanguageDirection
+{
+#pragma warning disable CA1069 // Enums values should not be duplicated
+    // ReSharper disable once InconsistentNaming
+    ltr = 0,
+    LeftToRight = 0,
+
+
+    // ReSharper disable once InconsistentNaming
+    rtl = 1,
+    RightToLeft = 1
+#pragma warning restore CA1069 // Enums values should not be duplicated
+}
