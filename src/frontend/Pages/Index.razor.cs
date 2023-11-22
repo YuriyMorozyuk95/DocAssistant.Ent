d734e2c1bf5be5e7b0787212eadcbf3370a1a712
YuriyMorozyuk95 <Yurii.Moroziuk@gunvorgroup.com>
2023-11-22 17:30:30 +0200

    .

---
+++ src/frontend/Pages/Index.razor.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+namespace ClientApp.Pages;
+
+public sealed partial class Index
+{
+    [Inject] public required ISessionStorageService SessionStorage { get; set; }
+
+    [Inject] public required ApiClient ApiClient { get; set; }
+}
