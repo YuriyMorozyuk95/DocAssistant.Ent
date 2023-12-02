fad7ee2c48917a1f5880195480252b4e3e379272
YuriyMorozyuk95 <Yurii.Moroziuk@gunvorgroup.com>
2023-12-02 16:08:02 +0200

    add button create new user

--- src/frontend/Pages/UsersPage.razor.cs
+++ src/frontend/Pages/UsersPage.razor.cs
@@ -4,6 +4,9 @@ namespace ClientApp.Pages;
 
 public sealed partial class UsersPage
 {
+    [Inject]
+    private NavigationManager NavigationManager { get; set; }
+
     public List<UserEntity> Users { get; set; } = default!;
     private UserEntity? _selectedUser;
 
